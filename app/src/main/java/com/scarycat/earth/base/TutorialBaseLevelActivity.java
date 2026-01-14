cedabdfad03a880c2c0323a7d1c985465ac07eac
JalpitParmar <jalpitparmar1234@gmail.com>
2026-01-14 16:48:16 +0530

    logic : Tutorial

--- app/src/main/java/com/scarycat/earth/base/TutorialBaseLevelActivity.java
+++ app/src/main/java/com/scarycat/earth/base/TutorialBaseLevelActivity.java
@@ -1,4 +1,63 @@
 package com.scarycat.earth.base;
 
-public class TutorialBaseLevelActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.scarycat.earth.R;
+
+public abstract class TutorialBaseLevelActivity extends BaseLevelActivity {
+
+    protected TextView txtTutorial;
+    protected int tutorialStep = 0;
+    protected boolean tutorialCompleted = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        // 🔒 Tutorial settings BEFORE engine starts
+        UNLIMITED_MOVES = true;
+        SCORE_ONLY = false;
+
+        super.onCreate(savedInstanceState);
+
+        txtTutorial = findViewById(R.id.txtTutorial);
+
+        startTutorial();
+    }
+
+    // ---------- TUTORIAL FLOW ----------
+    protected void startTutorial() {
+        disableInput = true;
+        showStep0();
+    }
+
+    protected void nextStep() {
+        tutorialStep++;
+
+        switch (tutorialStep) {
+            case 1:
+                showStep1();
+                break;
+            case 2:
+                showStep2();
+                break;
+            default:
+                finishTutorial();
+        }
+    }
+
+    protected void finishTutorial() {
+        tutorialCompleted = true;
+        disableInput = false;
+
+        if (txtTutorial != null) {
+            txtTutorial.setVisibility(View.GONE);
+        }
+    }
+
+    // ---------- STEPS (OVERRIDABLE) ----------
+    protected abstract void showStep0();
+    protected abstract void showStep1();
+    protected abstract void showStep2();
 }
